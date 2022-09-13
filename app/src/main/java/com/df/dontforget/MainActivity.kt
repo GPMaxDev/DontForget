c40db00bdc9f51c562778e41d54ca5bf2fedf010
GPMaxDev <gp.max.dev@gmail.com>
2022-09-14 01:20:44 +0300

    first commit

---
+++ app/src/main/java/com/df/dontforget/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.df.dontforget
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("hello")
+    }
+}
